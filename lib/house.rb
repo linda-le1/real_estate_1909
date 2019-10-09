cf7ad5e91d66abf929be0cb979eb1c119ec5e8a7
Linda Le <yueling1222@gmail.com>
2019-10-09 09:52:11 -0600

    Add add room methods and passing tests for if rooms exists in house and if rooms are added

--- lib/house.rb
+++ lib/house.rb
@@ -1,13 +1,21 @@
+require_relative "room"
+
+
 class House
 
   attr_reader :price,
-              :address
+              :address,
+              :rooms
 
-  def initialize (price, address)
+  def initialize (price, address, rooms)
     @price = price
     @address = address
+    @rooms =[]
   end
 
-
+  def add_room (room_category, length, width)
+  room = [room_category, length, width]
+    @rooms << room
+  end
 
 end
--- test/test_house.rb
+++ test/test_house.rb
@@ -6,18 +6,29 @@ require './lib/house'
 class TestHouse < Minitest::Test
 
   def setup
-    @house1 = House.new("$400000", "123 sugar lane")
+    @house_1 = House.new("$400000", "123 sugar lane", [])
   end
 
   def test_if_house_exists
-    assert_instance_of House, @house1
+    assert_instance_of House, @house_1
   end
 
   def test_if_house_has_price
-    assert_equal "$400000", @house1.price
+    assert_equal "$400000", @house_1.price
   end
 
   def test_if_house_has_address
-    assert_equal "123 sugar lane", @house1.address 
+    assert_equal "123 sugar lane", @house_1.address
   end
+
+  def test_if_house_has_rooms
+    assert_equal [], @house_1.rooms
+  end
+
+  def test_if_rooms_are_added
+    @house_1.add_room(:bedroom, 10, 13)
+    @house_1.add_room(:bedroom, 11, 15)
+    assert_equal [[:bedroom, 10, 13],[:bedroom, 11, 15]], @house_1.rooms
+  end
+
 end
